b032be86b87c473de01d2ba169771de40749f33a
milcent <marcelmilcent@gmail.com>
2014-12-22 08:33:12 -0200

    First Commit

---
+++ benford.py
@@ -0,0 +1,14 @@
+'''
+Author: Marcel Milcent
+
+This is a module for application of Benford's Law to a sequence of 
+numbers.
+
+Dependent on pandas and numpy
+'''
+import pandas as pd
+import numpy as np
+
+# create expected Benford distributions for the first digit, the 
+# second digit, and the first two digits
+
