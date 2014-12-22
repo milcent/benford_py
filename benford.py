53e1aa393bcd19f19fe90a56ec546931a2814bfb
milcent <marcelmilcent@gmail.com>
2014-12-22 08:37:23 -0200

    mattplotlib import

--- benford.py
+++ benford.py
@@ -4,11 +4,14 @@ Author: Marcel Milcent
 This is a module for application of Benford's Law to a sequence of 
 numbers.
 
-Dependent on pandas and numpy
+Dependent on pandas and numpy, using matplotlib for visualization
 '''
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # create expected Benford distributions for the first digit, the 
 # second digit, and the first two digits
+
+# create functions to run the tests
 
