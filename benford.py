c4bd5f68c04a4b95d510af94fb393035613405a2
milcent <marcelmilcent@gmail.com>
2015-01-20 01:03:36 -0200

    Expected Benford Distributions for the first, the second and the first two digits

--- benford.py
+++ benford.py
@@ -19,13 +19,52 @@ import matplotlib.pyplot as plt
 
 # create functions to run the tests
 
-def _get_mantissas_(arr):
+def _getMantissas_(arr):
 	'''
 	The mantissa is the non-integer part of the log of a number.
 	This fuction uses the element-wise array operations on numpy
 	to get the mantissas of each number's log.
 
-	arr: numpy array of integers or floats
+	arr: array of integers or floats >>> array of floats
 	'''
 
 	return np.log10(arr) - np.log10(arr).astype(int)
+
+# create arrays with expected Benford's distributions
+
+def firstDigit():
+	a = np.arange(1,10)
+	return np.log10(1 + (1. / a))
+
+
+def secondDigit():
+	a = np.arange(10,100)
+	aa = a.astype(str)
+	s = np.array([l[1] for l in aa]).astype(int)
+	b = np.arange(0,10)
+	probs = np.log10(1 + (1. / a))
+	c = np.zeros(10)
+	for n in b:
+		c[n] = probs[s == n].sum()
+	return c
+
+def firstTwoDigits():
+	a = np.arange(10,100)
+	return np.log10(1 + (1. / a))
+
+
+def lowUpBounds():
+
+def ________(arr, digits=2, dropLowerTen=True):
+	arr.sort()
+	if dropLowerTen == False:
+		# Multiply by constant to make all number with at least two
+		# digits left from the floating point.
+		# Take the second [1] element, should the first be 0, invert it
+		# and use the number of digits at the left to generate the power
+		# to elevate 10
+		p = len(str((1/arr[1]).astype(int)))	
+		arr *= 10**(p+1)
+	else:
+		arr = arr[arr>=10]
+
