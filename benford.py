c92d01e9a3b591f58b38df70d2ff821a8a2e7b79
milcent <marcelmilcent@gmail.com>
2015-01-22 20:53:52 -0200

    DataFrame indexing with the digits for the distributions

--- benford.py
+++ benford.py
@@ -20,7 +20,7 @@ def _getMantissas_(arr):
 	This fuction uses the element-wise array operations on numpy
 	to get the mantissas of each number's log.
 
-	arr: array of integers or floats >>> array of floats
+	arr: np.array of integers or floats >>> np.array of floats
 	'''
 
 	return np.log10(arr) - np.log10(arr).astype(int)
@@ -31,16 +31,21 @@ def firstDigit(output_DF=True):
 	Returns the expected probabilities of the first digits
 	according to Benford's distribution.
 	
-	- output_DF: Defaluts to Ture, Outputing a pandas Series
-				object with the probabilities and the respective
-				digits as the index, or a numpy array if False.
+	- output_DF: Defaluts to True, outputing a pandas DataFrame
+				with the probabilities, and the respective
+				digits as the index, or a numpy array if False, when
+				there will not be an index, and one must remember
+				that the indexing for the 1 probability will be 
+				[0], [1] for the two, and so on up untill [8] for
+				the probability of the first digit 9.
 	'''
-	a = np.arange(1,10)
-	b = np.log10(1 + (1. / a))
+	First_Dig = np.arange(1,10)
+	Expected = np.log10(1 + (1. / First_Dig))
 	if output_DF == False:
-		return b
+		return Expected
 	else:
-		return pd.Series(b, index = a)
+		return pd.DataFrame({'Expected':Expected,\
+			'First_Dig':First_Dig}).set_index('First_Dig')
 
 def secondDigit(output_DF=True):
 	'''
@@ -48,20 +53,24 @@ def secondDigit(output_DF=True):
 	according to Benford's distribution.
 	
 	output_DF: Defaluts to Ture, Outputing a pandas DataFrame
-	with the digit as index and the respective probability in
-	the 'prob' column, or a numpy array if False.
+			with the digit as index and the respective probability
+			in the 'prob' column, or a numpy array if False. In
+			this case, coincidently the indexing within the array
+			will match the second digits -> [0] - prob. of 0,
+			[1] - prob of 1, and sort	on up untill [9] - prob of 9.
 	'''
 	a = np.arange(10,100)
-	prob = np.log10(1 + (1. / a))
-	s = np.array(range(10)*9)
+	Expected = np.log10(1 + (1. / a))
+	Sec_Dig = np.array(range(10)*9)
 	if output_DF == False:
 		c = np.zeros(10)
-		for n in b:
-			c[n] = prob[s == n].sum()
+		for n in Sec_Dig:
+			c[n] = Expected[Sec_Dig == n].sum()
 		return c
 	else:
-		d = pd.DataFrame({'prob': prob, 'secdig': s}, index = a)
-		return d.groupby('secdig').agg(sum)
+		d = pd.DataFrame({'Expected': Expected, 'Sec_Dig': Sec_Dig},\
+		index = a)
+		return d.groupby('Sec_Dig').agg(sum)
 
 
 def firstTwoDigits(output_DF=True):
@@ -69,16 +78,17 @@ def firstTwoDigits(output_DF=True):
 	Returns the expected probabilities of the first two digits
 	according to Benford's distribution.
 	
-	- output_DF: Defaluts to Ture, Outputing a pandas Series
+	- output_DF: Defaluts to Ture, Outputing a pandas DataFrame
 				object with the probabilities and the respective
 				digits as the index, or a numpy array if False.
 	'''
-	a = np.arange(10,100)
-	b = np.log10(1 + (1. / a))
+	First_2_Dig = np.arange(10,100)
+	Expected = np.log10(1 + (1. / First_2_Dig))
 	if output_DF == False:
-		return b
+		return Expected
 	else:
-		return pd.Series(b, index = a)
+		return return pd.DataFrame({'First_2_Dig':First_2_Dig,\
+			'Expected':Expected}).set_index('First_2_Dig')
 
 
 def lowUpBounds():
