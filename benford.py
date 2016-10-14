3b968abd3a68042d35c4c380cd2d6e99c6808d7f
milcent <marcelmilcent@gmail.com>
2016-10-14 07:34:37 -0300

    license

--- benford.py
+++ benford.py
@@ -1,12 +1,25 @@
 '''
-Author: Marcel Milcent
-
-This is a module for application of Benford's Law to a sequence of 
-numbers.
+Benford_py for Python is a module for application of Benford's Law
+to a sequence of numbers.
 
 Dependent on pandas and numpy, using matplotlib for visualization
 
 All logarithms ar in base 10: "np.log10"
+
+Copyright (C) 2014  Marcel Milcent
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 # Imports
