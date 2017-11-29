a7aaa5a530bf1bb8eb3d3be29c28aa99955e091a
Leonardo Alcantara <leonardo.alcantara@gmail.com>
2017-11-29 11:31:11 -0200

    Changing the file structure to turn it into a Python Package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='bendford_py',
+      version='0.1',
+      description='A library for testing Bendford\'s Law data sets',
+      url='https://github.com/milcent/benford_py',
+      author='Marcel Milcent',
+      author_email='marcelmilcent@gmail.com',
+      license='GPLv3.0',
+      packages=['bendford'],
+      install_requires=[
+      	'pandas',
+      	'numpy',
+      	'matplotlib',
+      ],
+      zip_safe=False)
