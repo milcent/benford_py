3e2e27be5f8050192e68a80b3159582fec5cd92d
milcent <marcelmilcent@gmail.com>
2017-12-04 07:44:10 -0200

    Adjustments to setu.py setup.cfg for v0.1.0

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(name='bendford_py',
       version='0.1.0',
-      description='A library for testing Bendford\'s Law data sets',
+      description='A library for testing data sets with Bendford\'s Law',
       url='https://github.com/milcent/benford_py',
+      download_url=
+        'https://github.com/milcent/benford_py/archive/v0.1.0.tar.gz',
       author='Marcel Milcent',
       author_email='marcelmilcent@gmail.com',
       license='GPLv3.0',
