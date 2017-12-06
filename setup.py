98b82c150ce89cc21cc5c54386b9c0d96999d905
milcent <marcelmilcent@gmail.com>
2017-12-06 07:31:04 -0200

    Updated setup.py and demo notebook to package status

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(name='benford_py',
-      version='0.1.0',
+      version='0.1.0.1',
       description='A library for testing data sets with Bendford\'s Law',
       url='https://github.com/milcent/benford_py',
       download_url=
-        'https://github.com/milcent/benford_py/archive/v0.1.0.tar.gz',
+        'https://github.com/milcent/benford_py/archive/v.0.1.0.1.tar.gz',
       author='Marcel Milcent',
       author_email='marcelmilcent@gmail.com',
       license='GPLv3.0',
-      packages=['benford_py'],
+      packages=['benford'],
       install_requires=[
       	'pandas',
       	'numpy',
