a5cd923276ebf0c8175952750646613b66af779f
Marcel Milcent <marcelmilcent@gmail.com>
2019-11-12 05:34:06 -0300

    __init_.py house cleaning

--- benford/__init__.py
+++ benford/__init__.py
@@ -1,10 +1,3 @@
-from __future__ import print_function, division
-
-
-# import pandas as pd
-# import numpy as np
-# import matplotlib.pyplot as plt
-
 from .benford import *
 
 __version__ = '0.1.0.4'
