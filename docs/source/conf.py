7091e82eb6ca82d97a58ee175b35a2009b501ad7
Marcel Milcent <marcelmilcent@gmail.com>
2020-01-30 01:05:49 -0300

    napoleon extension name for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ release = '0.2.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
