860ebf7f504d2c9e28ccb1ccbab7645167f5883f
Marcel Milcent <marcelmilcent@gmail.com>
2020-12-01 21:31:01 -0300

    updated version

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 '''Benfords law module'''
-__version__ = "0.2.6"
+__version__ = "0.3.0"
--- benford/__init__.py
+++ benford/__init__.py
@@ -24,4 +24,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from .benford import *
 
-__version__ = '0.2.7'
+__version__ = '0.3.0'
