acedd699ff3f88c1b6c43e2a8645e6d4ae723502
Marcel Milcent <marcelmilcent@gmail.com>
2020-12-19 12:01:28 -0300

    import order setup.py (pylint)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 ''' Setup Pyt for benford's module'''
+from os import path
 from setuptools import setup
 
-from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README-pypi.md'), encoding='utf-8') as f:
     long_description = f.read()
