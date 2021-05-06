edb0b2b4fed2d2b3a76a4cf7380efeab6c9653eb
Marcel Milcent <marcelmilcent@gmail.com>
2021-05-06 08:09:29 -0300

    new digits submodule

---
+++ benford/digits.py
@@ -0,0 +1,17 @@
+class _Ingestor_:
+    pass
+
+class _Digits_:
+    pass
+
+class DigitsTest:
+    pass
+
+class SecondOrderDigitsTest:
+    pass
+
+class SummationTest:
+    pass
+
+class MantissasTest:
+    pass
